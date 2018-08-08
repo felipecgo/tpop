0020b9a9a0fd0b9752cbb3d7f5bfa6d1492a034b
Felipe <felipecgo@hotmail.com>
2018-08-08 14:52:45 -0300

    Adicionando chapter_1 e chapter_2

--- chapter_2/p31.c
+++ chapter_2/p31.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Nameval Nameval;
-
 struct Nameval
 {
 	char *name;
 	int value;
 };
 
-Nameval htmlchars[] =
+Nameval htmlchars[] = 
 {
 	"AElig", 0x00c6,
 	"Aacute", 0x00c1,
@@ -17,18 +18,31 @@ Nameval htmlchars[] =
 	"zeta", 0x03b6,
 };
 
-void print_Nameval(const Nameval *nameval)
+/* Lookup: binary seach for name in tab; return index */
+int lookup(char *name, Nameval tab[], int ntab)
 {
-	printf("Name: %s\t\t\tValue: 0x%04x\n", nameval->name, nameval->value);
+	int low, high, mid, cmp;
+	
+	low = 0;
+	high = ntab - 1;
+	while(low <= high)
+	{
+		mid = (low+high) / 2;
+		cmp = strcmp(name, tab[mid].name);
+		if (cmp < 0)
+			high = mid - 1;
+		else if (cmp > 0)
+			low = mid + 1;
+		else	/* Found match */
+			return mid;
+	}
+	
+	return -1; /* No match */
 }
 
 int main()
 {
+	printf("Posição %d\n", lookup("Aciarc", htmlchars, 4));
 	
-	for (int i=0; i < sizeof(htmlchars)/sizeof(htmlchars[0]); i++)
-	{
-		print_Nameval(&htmlchars[i]);
-	}
-							 
 	return 0;
 }
